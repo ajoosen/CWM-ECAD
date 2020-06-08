b8c3f12663c2048907696fbb9a4b412319046ce1
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-08 14:44:10 +0000

    editing top.v for ex2

--- Ex2/top.v
+++ Ex2/top.v
@@ -22,11 +22,23 @@
 
 module mux(
     //Todo: define inputs here
+    input a,
+    input b,
+    input sel,
+    input clock.
+    
+    output out
     );
     
     //Todo: define registers and wires here
-
+    wire   out;
 
     //Todo: define your logic here                 
-      
+    always @(posedge clock)
+        assign #5 out = a ? (sel==1'b0):
+            b ? (sel==1'b1):
+          //  default case
+            2'b1;
+
+
 endmodule
