e1413f00c3e5183343e6f5c21fc2ba9a6f58bad7
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-10 11:15:14 +0000

    solution to Ex 5 passed test

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name: Artjom Joosen
+// Date: 10.06.2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -12,4 +12,50 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module traffic_lights_top(
+    //Todo: define inputs here
+    input clk,
+    output reg red,
+    output reg amber,
+    output reg green
+
+    );
+
+	    always@(posedge clk)
+			//if the state is valid
+		    if   ( (red==1'b0 && amber==1'b0 && green==1'b0) || (red==1'b1 && amber==1'b0 && green==1'b1) || (red==1'b0 && amber==1'b1 && green==1'b1) || (red==1'b1 && amber==1'b1 && green==1'b1) ) 
+			    begin       //start with red and goes clockwise around state graph
+				red <= 1'b1;
+				amber <= 1'b0;
+				green <= 1'b0;
+			    end else 
+			    if(red==1'b1 && amber==1'b0 && green==1'b0)
+			    begin
+				red <= 1'b1;
+				amber <= 1'b1;
+				green <= 1'b0;
+			    end else 
+                            if(red==1'b1 && amber==1'b1 && green==1'b0)
+		            begin
+				red <= 1'b0;
+				amber <= 1'b0;
+				green <= 1'b1;
+			    end else 
+			    if(red==1'b0 && amber==1'b0 && green==1'b1)
+		            begin
+				red <= 1'b0;
+				amber <= 1'b1;
+				green <= 1'b0;
+			    end 
+		    else //accounts for non-valid states -- will just go to initial state (rag == 100)
+                    begin
+			red <= 1'b1;
+			amber <= 1'b0;
+			green <= 1'b0;
+		    end
+
+endmodule
 
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,72 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Traffic Lights
-// Student Name:
-// Date: 
+// Student Name: Artjom Joosen
+// Date: 09/06/2020
 //
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+
+//Todo: Parameters
+    parameter CLK_PERIOD = 10;
+
+//Todo: Registers and wires
+    reg clk;
+    reg [2:0] previous_rag_state;
+    wire red;
+    wire amber;
+    wire green;
+    reg err;
+
+
+//Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+    
+ //Todo: User logic
+	initial begin
+       	err=0;
+	previous_rag_state = {red,amber,green};	   
+    	clk = 0;
+
+	#CLK_PERIOD;
+	forever begin
+		previous_rag_state = {red,amber,green};	   
+		
+		//check for forbidden states
+		#CLK_PERIOD if ((red==1'b0 && amber==1'b0 && green==1'b0) || (red==1'b1 && amber==1'b0 && green==1'b1) || (red==1'b1 && amber==1'b1 && green==1'b1) || (red==1'b0 && amber==1'b1 && green==1'b1)) 
+			begin
+   	   		   $display("Error! forbidden state!");
+   	   		   err=1;
+       			end
+
+		if (previous_rag_state=={red,amber,green})
+			begin
+   	   		   $display("Error! State not incremented!");
+   	   		   err=1;
+       		        end	
+		end
+	end
+
+//Todo: Finish test, check for success
+      initial begin
+        #100 if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+
+//User's module
+    traffic_lights_top top (.clk(clk), .red(red), .amber(amber), .green(green) );
+     
+endmodule
