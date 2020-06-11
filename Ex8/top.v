cace0dd8ab23792ebb6e4195627066741af1fea5
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-11 14:46:11 +0000

    still getting Multiplication Error

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,10 +1,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
+// Student Name:ement a times table of 0..7x0..7
+//  using a memory and AXI-4-lite interface.
 // Date: 
 //
-//  Description: In this exercise, you need to implement a times table of 0..7x0..7
-//  using a memory and AXI-4-lite interface.
+//  Description: In this exercise, you need to impl
 //
 //  inputs:
 //           clk, rst, a[2:0], b[2:0], read
@@ -12,3 +12,56 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns / 100ps
+
+module times_table (
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	input enable,
+	input rst,
+	output wire [5:0] result
+	);
+
+	wire [32:0] addr;
+	wire awready;
+	wire rsta_busy;
+	wire rstb_busy;
+	wire wready;
+	wire [1:0] bresp;
+	wire bvalid;
+	wire arready;
+	wire rvalid;
+	wire s_axi_rresp;
+	
+	
+	assign addr = {24'd0,a,b,2'd0};
+
+	blk_mem_gen_0 Axi4_multiply (
+	  .rsta_busy(rsta_busy),          
+	  .rstb_busy(rstb_busy),          
+	  .s_aclk(clk),                
+	  .s_aresetn(rst),      
+	    
+	  .s_axi_awaddr(0),    
+	  .s_axi_awvalid(0),  
+	  .s_axi_awready(awready),  
+	  .s_axi_wdata(0),      
+	  .s_axi_wstrb(0),      
+	  .s_axi_wvalid(0),    
+	  .s_axi_wready(wready),   
+	  .s_axi_bresp(bresp),      
+	  .s_axi_bvalid(bvalid),    
+	  .s_axi_bready(0),    
+	  .s_axi_araddr(addr),    
+	  .s_axi_arvalid(1),  
+	  .s_axi_arready(arready),  
+	  .s_axi_rdata(result),      
+	  .s_axi_rresp(s_axi_rresp),     
+	  .s_axi_rvalid(rvalid),    
+	  .s_axi_rready(enable)    
+	);
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -7,3 +7,84 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+//Parameters
+    parameter CLK_PERIOD = 10;
+
+//Registers and wires
+    	reg clk;
+	reg err;
+	reg rst;
+	reg [2:0] a;
+	reg [2:0] b;
+	reg enable;
+	reg [5:0] result_prev;
+	wire [5:0] result;
+
+//Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
+    
+ //Todo: User logic
+	initial begin
+	       	err=0; 
+		enable = 1;
+		a = 0;
+		b = 0;
+		rst = 0;
+		#(2*CLK_PERIOD)	rst = 1;
+		result_prev = result;
+
+       forever begin
+       #(2*CLK_PERIOD) if ((enable==0) & (result!=result_prev))
+		begin
+           		$display("***TEST FAILED! Enable error***");
+           		err=1;
+		end
+		if ((enable==1) & (result != a * b))
+		begin
+			$display("***TEST FAILED! Multiplication error***");
+			err=1;
+		end
+
+		if (a == 3'b010 & b == 3'b010)
+			enable = !enable;
+		if (a == 3'b011 & b == 3'b010)
+			enable = 1;
+		if (a<3'b111)
+			a = a+1;
+		else
+			a = 0;
+
+		result_prev=result;
+end
+end
+
+initial begin
+	#34 forever begin
+		#240
+		b=b+1;
+	end
+end
+
+
+//Todo: Finish test, check for success
+      initial begin
+      #(500*CLK_PERIOD) if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+    //User's module
+    times_table top (.clk(clk), .enable(enable), .a(a),	.b(b), .result(result),	.rst(rst) );
+     
+endmodule 
+
