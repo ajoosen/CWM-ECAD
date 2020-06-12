04b6e81852009467d7bf3b60085a6e620f5a5f4e
Cloud User <centos@ip-172-31-2-218.eu-west-2.compute.internal>
2020-06-12 15:23:17 +0000

    work in progress

--- Ex9/top.v
+++ Ex9/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #9 
-// Student Name:
-// Date: 
+// Student Name: Artjom Joosen
+// Date: 12.06.2020
 //
 //  Description: In this exercise, you need to design a module that lights leds
 //  at different frequencies
@@ -17,11 +17,17 @@
 module top(
     input clk_p,
     input clk_n,
-     //Todo: add all other ports besides clk_n and clk_p 
+
+//Todo: everything other than clk_n and clk_p 
+	input rst_n,	
+	input button,
+	output reg led_0,
+	output reg led_1,
+	output reg led_2
    );
     
 
-   /* clock infrastructure, do not modify */
+/* clock infrastructure, do not modify */
         wire clk_ibufds;
 
     IBUFDS IBUFDS_sysclk (
@@ -38,5 +44,33 @@ module top(
       );
 
 //Add logic here
+	reg [22:0] register;
+
+	always @(posedge clk) 
+		begin
+		if ((rst_n == 0) | (button == 1'b1)) 
+		begin
+			led_0=0;
+			led_1=0;
+			led_2=0;
+			register=0;
+		end
+		else 
+		begin
+			if (register == 21'b111111111111111111111) 
+				led_0=~led_0;
+			if (register == 22'b1111111111111111111111) 
+				led_1=~led_1;
+			if (register == 23'b11111111111111111111111) 
+			begin
+				led_2=~led_2;
+				register=0;
+			end
+			else
+				register=register+1;
+		end
+
+	end
+
 
 endmodule
